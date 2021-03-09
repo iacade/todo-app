3eb274935fb3d5cf1e9c31fe913774b241e0f555
iacade <iacade.drozdov@gmail.com>
2021-03-10 01:05:22 +0200

    Update a11y styles

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { classes } from "../../helpers/classes";
 
 function Input(props) {
+    const [ focused, setFocused ] = useState(false);
     const [ value, setValue ] = useState(props.defaultValue || "");
     let realValue = null;
     let handleChange = null;
@@ -19,15 +20,19 @@ function Input(props) {
 
     const className = classes({
         "x-input": true,
+        "x-input--focus": focused,
         "x-input--not-empty": realValue
     });
 
     return (
         <label className={ className }>
             <span className="x-input__placeholder">{ props.placeholder || props.name }</span>
+            <span className="x-input__border"></span>
             <input type="text"
                 name={ props.name }
                 value={ realValue }
+                onFocus={ () => setFocused(true) }
+                onBlur={ () => setFocused(false) }
                 onChange={ handleChange }
                 onKeyDown={ props.onKeyDown } />
         </label>
